ccaab7e090b787a5178bbfbbbece5fe66487e0ba
winnie-ma <xuma8821@gmail.com>
2023-10-02 03:43:04 +1100

    feat: add redirect feature after creating blog

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,13 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("Winnie");
   const [isPending, setIsPending] = useState(false);
+  //can be used to go for/backward or redirect
+  const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -17,6 +20,7 @@ const Create = () => {
     }).then(() => {
       console.log("new blog added");
       setIsPending(false);
+      history.push("/");
     });
   };
   return (

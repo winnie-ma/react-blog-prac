ec38f95a47d073708307b39468e61a34b751d32f
winnie-ma <xuma8821@gmail.com>
2023-10-02 01:08:01 +1100

    feat: add blog homepage and fake api server with json data

---
+++ src/Home.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
+const Home = () => {
+  const [blogs, setBlogs] = useState(null);
+  useEffect(() => {
+    fetch("http://localhost:8000/blogs")
+      .then((res) => res.json())
+      .then((data) => {
+        setBlogs(data);
+      });
+  }, []);
+  return (
+    <div className="home">
+      {blogs && <BlogList blogs={blogs} title="All blogs!" />}
+    </div>
+  );
+};
+
+export default Home;

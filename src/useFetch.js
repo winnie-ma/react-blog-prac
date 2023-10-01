fc0d145ec6b0129b9477780d159d56389401879e
winnie-ma <xuma8821@gmail.com>
2023-10-02 02:43:39 +1100

    feat: add cleanup function to the useEffect and add route

--- src/useFetch.js
+++ src/useFetch.js
@@ -4,9 +4,11 @@ const useFetch = (url) => {
   const [isPending, setIsPending] = useState(true);
   const [error, setError] = useState(null);
   useEffect(() => {
+    const abortCont = new AbortController();
     //simulate the real api request delay
     setTimeout(() => {
-      fetch(url)
+      //Assocciate abort contoller with this fetch
+      fetch(url, { signal: abortCont.signal })
         .then((res) => {
           if (!res.ok) {
             //check for api return not success error, like not exist ect, it will then be catched with msg
@@ -15,16 +17,25 @@ const useFetch = (url) => {
           return res.json();
         })
         .then((data) => {
+          console.log(data);
           setData(data);
           setIsPending(false);
           setError(null);
         })
         .catch((err) => {
-          //catch network connection error
-          setError(err.message);
-          setIsPending(false);
+          console.log(err);
+          if (err.name === "AbortError") {
+            //when abort and stop fetch, it will trigger the catch, so judge here
+            console.log("Fetch aborted");
+          } else {
+            //catch network connection error
+            setError(err.message);
+            setIsPending(false);
+          }
         });
     }, 1000);
+    //cleanup function that stop the associated fetch
+    return () => abortCont.abort();
   }, [url]);
   return { data, isPending, error };
 };

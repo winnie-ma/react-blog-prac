bffc94462b6f12078541a633bd5b69c6d4789195
winnie-ma <xuma8821@gmail.com>
2023-10-02 03:50:00 +1100

    feat: add delete blog feature

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 const BlogDetails = () => {
   const { id } = useParams();
@@ -7,6 +7,14 @@ const BlogDetails = () => {
     isPending,
     error,
   } = useFetch(`http://localhost:8000/blogs/${id}`);
+  const history = useHistory();
+  const handleClick = () => {
+    fetch(`http://localhost:8000/blogs/${id}`, {
+      method: "DELETE",
+    }).then(() => {
+      history.push("/");
+    });
+  };
   return (
     <div className="blog-details">
       {isPending && <div>Loading...</div>}
@@ -16,6 +24,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleClick}>delete</button>
         </article>
       )}
     </div>
